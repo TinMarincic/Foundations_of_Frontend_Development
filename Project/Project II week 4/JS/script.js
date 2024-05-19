2a09e2b8752f2a82b46d5bb7ee504150aca6739d
TinMarincic <tinmarincic2004@gmail.com>
2024-05-19 15:19:50 +0200

    added gallery and sorted files into folders for better organisation

--- Project/Project II week 4/JS/script.js
+++ Project/Project II week 4/JS/script.js
@@ -3,19 +3,19 @@ var isDarkTheme = localStorage.getItem('isDarkTheme');
 
 if (isDarkTheme === 'true') {
     document.body.classList.add("dark-theme");
-    icon.src = "moon.png";
+    icon.src = "/images/moon.png";
 } else {
     document.body.classList.remove("dark-theme");
-    icon.src = "sun.png";
+    icon.src = "/images/sun.png";
 }
 
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
         localStorage.setItem('isDarkTheme', 'true');
-        icon.src = "moon.png";
+        icon.src = "/images/moon.png";
     } else {
         localStorage.setItem('isDarkTheme', 'false');
-        icon.src = "sun.png";
+        icon.src = "/images/sun.png";
     }
 }
