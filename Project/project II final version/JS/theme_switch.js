3578752922022d3b19ed9648282d0bfb455c7906
TinMarincic <tinmarincic2004@gmail.com>
2024-06-02 11:25:30 +0200

    Final version of Project II

---
+++ Project/project II final version/JS/theme_switch.js
@@ -0,0 +1,21 @@
+var icon = document.getElementById("icon");
+var isDarkTheme = localStorage.getItem('isDarkTheme');
+
+if (isDarkTheme === 'true') {
+    document.body.classList.add("dark-theme");
+    icon.src = "/images/sun.png";
+} else {
+    document.body.classList.remove("dark-theme");
+    icon.src = "/images/moon.png";
+}
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        localStorage.setItem('isDarkTheme', 'true');
+        icon.src = "/images/sun.png";
+    } else {
+        localStorage.setItem('isDarkTheme', 'false');
+        icon.src = "/images/moon.png";
+    }
+}
